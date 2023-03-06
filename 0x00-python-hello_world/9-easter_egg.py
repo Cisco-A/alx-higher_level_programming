20cb186a93c6bd79f051845acd8af04183a6c32b
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-06 15:15:32 -0800

    Task 9

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+print("The Zen of Python, by Tim Peters\n")
+print("Beautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.")
+print("Complex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.")
+print("Readability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.")
+print("Errors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.")
+print("There should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.")
+print("Although never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.")
+print("Namespaces are one honking great idea -- let's do more of those!")
