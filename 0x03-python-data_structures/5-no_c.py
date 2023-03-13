509336036beda47e065ade35a600fee67131caf8
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-12 18:51:49 -0700

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_list = []
+    # convert string into list
+    for char in my_string:
+        new_list.append(char)
+    # remove instances of "C" or "c"
+    for i in range(len(new_list)):
+        if new_list[i] == "c" or new_list[i] == "C":
+            new_list.pop(i)
+        
+        new_string = ""
+        return (new_string.join(new_list))
