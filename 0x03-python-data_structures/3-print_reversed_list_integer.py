acc246983c074aba423c5c4cdad739a106392d95
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-12 18:16:16 -0700

    Task 3 correction

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        #current index in the loop is subtracted from length of list
-        #to access last element on the list
-        #1 is then subtracted from the result to stay in range of index
+        # current index in the loop is subtracted from length of list
+        # to access last element on the list
+        # 1 is then subtracted from the result to stay in range of index
         print("{:d}".format(my_list[(len(my_list) - i) - 1]))
