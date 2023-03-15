7b5c1a74006340a4560a5a9b618fb0c958642762
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-14 19:42:33 -0700

    Task 7 corection

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == len(tuple_b) == 2:
-        return(tuple_a + tuple_b)
-    elif len(tuple_a) > 2:
-        return(tuple_a[:1] + tuple_b)
-    elif len(tuple_b) > 2:
-        return(tuple_a + tuple_b[:1])
-    elif len(tuple_a) < 2:
-        tuple_1 = list(tuple_a)
+    if len(tuple_a) < 2:
         if len(tuple_a) == 0:
-            tuple_1.append(0)
-            tuple_1.append(0)
+            tuple_a = 0, 0
         else:
-            tuple_1.append(0)
-        tuple_a = tuple(tuple_1)
-        return(tuple_a + tuple_b)
-    elif len(tuple_b) < 2:
-        tuple_2 = list(tuple_b)
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
         if len(tuple_b) == 0:
-            tuple_2.append(0)
-            tuple_2.append(0)
+            tuple_b = 0, 0
         else:
-            tuple_2.append(0)
-        tuple_b = tuple(tuple_2)
-        return(tuple_a + tuple_b)
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
