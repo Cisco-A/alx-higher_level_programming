ceb21c8dbcf00881e15e7236399192d95ce406ae
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-14 20:46:01 -0700

    Task 5 and 6 correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    '''
     new_list = []
     # convert string into list
     for char in my_string:
@@ -7,6 +8,9 @@ def no_c(my_string):
     # remove instances of "C" or "c"
     for i in range(len(new_list)):
         if new_list[i] == "c" or new_list[i] == "C":
-            new_list.pop(i) 
+            new_list.pop(i)
     new_string = ""
     return (new_string.join(new_list))
+    '''
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
