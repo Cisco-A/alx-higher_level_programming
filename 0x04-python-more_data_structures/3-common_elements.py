e0379adfa149e069728cf1a77136cd208640a666
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-15 18:28:07 -0700

    Task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = lambda a, b: a if a == b else None
-    return (list(map(common, set_1, set_2)))
+    return (list(map(lambda a, b: a if a == b else None, set_1, set_2)))
