c15e112ce9a33db746f2e6742fb353bebbf0c2ff
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-03-15 18:13:21 -0700

    task 2 correction

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    #Adds all unique integers in a list
+    # Adds all unique integers in a list
     uniq_set = set(my_list)
     sum = 0
     for item in uniq_set:
-        sum =+ item
+        sum += item
     return sum
