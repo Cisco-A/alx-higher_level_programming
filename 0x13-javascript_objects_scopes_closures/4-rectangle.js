c0a7eb4917f1388e92c94a9d45dddec192224182
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-06-14 19:20:35 -0700

    Added new instances: rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const row = 'X'.repeat(this.width);
+    let i = 0;
+    while (i < this.height) {
+      console.log(row);
+      i++;
+    }
+  }
+
+  rotate () {
+    const c = this.width;
+    this.width = this.height;
+    this.height = c;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
