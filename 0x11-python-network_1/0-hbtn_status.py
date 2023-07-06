1628165a95feb6d9c1edc800884248bc4a1d6357
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-07-06 19:50:06 +0100

    Formatted to pycodestyle v2.8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,9 @@
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
-    # print(
-    #     f"Body response:\n    - type: {type(html)}\n    - content: {html}\n    - utf8 content: {html.decode('utf8')}")
 
     print("Body response:")
     print("\t- type: {}".format(type(html)))
