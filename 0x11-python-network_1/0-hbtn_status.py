67076652f55dc72104def2419583c69b447cde51
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-07-06 19:36:31 +0100

    Changed format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,10 @@ if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         html = response.read()
-    print(
-        f"Body response:\n    - type: {type(html)}\n    - content: {html}\n    - utf8 content: {html.decode('utf8')}")
+    # print(
+    #     f"Body response:\n    - type: {type(html)}\n    - content: {html}\n    - utf8 content: {html.decode('utf8')}")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
